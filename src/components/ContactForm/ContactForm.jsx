3c8cde848a62d05bbc4230b55d79a74a8726039d
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-06-20 22:02:34 +0300

    fixed Field

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -11,12 +11,24 @@ export default function ContactForm() {
     name: "",
     number: ""
   };
+  // const userSchema = Yup.object().shape({
+  //   name: Yup.string().min(3, "Too Short!")
+  //     .max(50, "Too Long!").required("Required"),
+  //   number: Yup.string().min(3, "Too Short!")
+  //     .max(50, "Too Long!").required("Required")
+  // });
+
   const userSchema = Yup.object().shape({
-    name: Yup.string().min(3, "Too Short!")
-      .max(50, "Too Long!").required("Required"),
-    number: Yup.string().min(3, "Too Short!")
-      .max(50, "Too Long!").required("Required")
+    name: Yup.string()
+      .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed")
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    number: Yup.string()
+      .matches(/^\d{3}-\d{3}-\d{2}-\d{2}$/, "Invalid phone number format")
+      .required("Required")
   });
+
     const fieldId = useId();
     const dispatch = useDispatch();
     
@@ -37,13 +49,13 @@ export default function ContactForm() {
         <Form className={css.form}>
           <div className={css.wrapper}>
             <label  htmlFor={`${fieldId}-name`}>Name</label>
-            <Field className={css.field} type="text" name="name" id={`${fieldId}-name`}/>
+            <Field className={css.field} type="text" name="name" id={`${fieldId}-name`} placeholder="Marina" />
             <ErrorMessage name="name" component="span" className={css.error}/>
           </div>
           <div className={css.wrapper}> 
             <label  htmlFor={`${fieldId}-number`}>Number</label>
-            <Field className={css.field} type="text" name="number" id={`${fieldId}-number`}/>
-            <ErrorMessage name="number" component="span" className={css.error}/>
+            <Field className={css.field} type="text" name="number" id={`${fieldId}-number`}  placeholder="050-123-45-67" />
+            <ErrorMessage name="number" component="span" className={css.error} />
           </div>
           <button className={css.btn} type="submit">Add contact</button>
         </Form>
