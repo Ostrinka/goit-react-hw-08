9a7263fc8207ef9101d33eb3b4b731f7a19f3949
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-06-20 14:11:54 +0300

    add pages

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import PageTitle from "../../components/PageTitle/PageTitle";
+
+export default function HomePage() {
+  return (
+    <div>
+      <PageTitle>
+        Welcome To Contact Book{" "}
+        <span role="img" aria-label="Greeting icon">
+          💁‍♀️
+        </span>
+      </PageTitle>
+    </div>
+  );
+}
